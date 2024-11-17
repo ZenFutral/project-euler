698461e9d41d974e2a3c7e66b35bc1ac283fb6e8
Zen Futral <zen.futral@gmail.com>
2024-11-17 15:49:28 -0700

    Update Problem 1.py

--- Problem 1.py
+++ Problem 1.py
@@ -1,18 +1,33 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 # Answer: 233168
+# Average Runtime: 0.002s
 
-# I wanted to use this problem as an excuse to jam everything into nested list comprehension.
+from time import time
+from statistics import mean
 
-limit = 1000
-divisors = (3, 5)
-multiples = []
+runCount = 10
+runDurations = []
+
+def run():
+    limit : int = 1000
+    divisors : tuple[int] = (3, 5)
+    multiples : list[int] = []
 
 
-[[multiples.append(num) for divisor in divisors 
-    if (num % divisor == 0) and (num not in multiples)] 
-    for num in range(limit)]
+    [[multiples.append(num) for divisor in divisors 
+        if (num % divisor == 0) and (num not in multiples)] 
+        for num in range(limit)]
 
-answer = sum(multiples)
+    answer : int = sum(multiples)
+    return answer
 
-print(answer)
+
+for count in range(runCount):
+    startTime = time()
+    answer = run()
+    runDurations.append(time() - startTime)
+
+averageRuntime = mean(runDurations)
+print(f"Answer: {answer}")
+print(f"Runtime: {round(averageRuntime, 3)}s")
