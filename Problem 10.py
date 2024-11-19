58dca7dca334323db25a3caf5a017d4661c52b13
Zen Futral <zen.futral@gmail.com>
2024-11-19 05:47:07 -0700

    Update Problem 10.py

--- Problem 10.py
+++ Problem 10.py
@@ -2,34 +2,52 @@
 # Find the sum of all the primes below two million.
 #
 # Answer: 142913828922
+# Average Runtime: 0.321s
 
-def isPrime(number : int, primeList : int):       
-    for prime in primeList:
-        if number % prime == 0:
-            return False
-        
-    if number == 1:
-        return False
+from time import time
+from statistics import mean
+from numpy import arange, ones, insert
+
+def findPrimes(upperBound : int):
+    primeArray = arange(3, upperBound, 2)       # 3, 5, 7, 9, 11...upperBound 
+
+    # Since our prime array starts at 3, if we begin elimanating multiples and iterate by a step each time, we should always have a prime next
+    # Therefor, prime conditions are that it is not divisable by a divisor OR it is the number being tested
+
+    is_prime = ones(len(primeArray), dtype=bool)
+
+    for i, number in enumerate(primeArray):
+        if is_prime[i]:  # Check if the number is still a potential prime
+            # Filter out multiples of the current prime
+            is_prime[i+number::number] = False
     
-    return True
+    # Applies bool mask to initial array
+    primeArray = primeArray[is_prime]
 
-def findPrimes(targetNumber : int):
-    primeList = [2, 3]
-    numberToTest = 3
-
-    while numberToTest < targetNumber:
-        numberToTest += 2
-
-        if isPrime(numberToTest, primeList):
-            print(numberToTest)
-            primeList.append(numberToTest)
-                             
-    return primeList
+    # Inserts 2 at the begining, since we skipped that originally.
+    primeArray = insert(primeArray, 0, 2)
+    return primeArray
 
 def run():
     targetNumber = 2000000
     primeList = findPrimes(targetNumber)
     answer = sum(primeList)
-    print(answer)
+    return answer
 
-run()
+# ================================
+# NO PROBLEM LOGIC BELOW THIS LINE
+# ================================
+
+runCount = 10
+runDurations = []
+
+for count in range(runCount):
+    print(f"Run: {count + 1}")
+    startTime = time()
+    answer = run()
+    runDurations.append(time() - startTime)
+
+averageRuntime = mean(runDurations)
+print(f"Answer: {answer}")
+print(f"Runtime: {round(averageRuntime, 3)}s")
+    
