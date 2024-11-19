970f9b19d227ef99f6bfd1c95ef6d59b5b654e6f
Zen Futral <zen.futral@gmail.com>
2024-11-19 05:38:58 -0700

    Update Problem 7.py

--- Problem 7.py
+++ Problem 7.py
@@ -1,6 +1,10 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 # What is the 10,001st prime number?
 # Answer: 104743
+# Average Runtime: 3.372s
+
+from time import time
+from statistics import mean
 
 def isPrime(number : int, primeList : int):       
     for prime in primeList:
@@ -30,5 +34,20 @@ def run(targetLength : int):
     answer : int = primeList[-1]
     return answer
 
-answer : int = run(10001)
-print(answer)
+# ================================
+# NO PROBLEM LOGIC BELOW THIS LINE
+# ================================
+
+runCount = 10
+runDurations = []
+
+for count in range(runCount):
+    print(f"Run: {count + 1}")
+    startTime = time()
+    answer = run(10001)
+    runDurations.append(time() - startTime)
+
+averageRuntime = mean(runDurations)
+print(f"Answer: {answer}")
+print(f"Runtime: {round(averageRuntime, 3)}s")
+    
