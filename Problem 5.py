c5158e5cf757fd9819ad14ba85b145bf090b9cc4
Zen Futral <zen.futral@gmail.com>
2024-11-19 05:34:44 -0700

    Update Problem 5.py

--- Problem 5.py
+++ Problem 5.py
@@ -2,9 +2,15 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisable by all of the numbers from 1 to 20?
 # Answer: 232792560
+# Average Runtime: 2.866s
+
+from time import time
+from statistics import mean
 
 
-def run(numberToTest = 0, maxDivider = 20):
+def run():
+    maxDivider = 20
+    numberToTest = 0
     while True:
         numberToTest += maxDivider  # Counting by the maxDivider maximized how many numbers we can skip
 
@@ -16,8 +22,23 @@ def run(numberToTest = 0, maxDivider = 20):
                 break
 
             elif divider == 2:
-                answer : int = numberToTest
+                answer = numberToTest
                 return answer
 
-answer : int = run()
-print(answer)
+# ================================
+# NO PROBLEM LOGIC BELOW THIS LINE
+# ================================
+
+runCount = 10
+runDurations = []
+
+for count in range(runCount):
+    print(f"Run: {count + 1}")
+    startTime = time()
+    answer = run()
+    runDurations.append(time() - startTime)
+
+averageRuntime = mean(runDurations)
+print(f"Answer: {answer}")
+print(f"Runtime: {round(averageRuntime, 3)}s")
+    
