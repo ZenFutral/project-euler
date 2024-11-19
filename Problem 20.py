c749fb2360df4452f4bbe33c7b6a6db7f772bfc1
Zen Futral <zen.futral@gmail.com>
2024-11-19 05:51:12 -0700

    Update Problem 20.py

--- Problem 20.py
+++ Problem 20.py
@@ -1,4 +1,14 @@
+# n! means n x (n-1) x...x 3 x 2 x 1
+# For example, 10! = 10 x 9 x...x 3 x 2 x 1 = 3628800
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27
+# Find the sum of the digits in the number 100!.
+# Answer: 648
+# Average Runtime: 0.0s
+
 from math import factorial
+from time import time
+from statistics import mean
+
 
 def run():
     number = 100
@@ -10,6 +20,20 @@ def run():
 
     return answer
 
+# ================================
+# NO PROBLEM LOGIC BELOW THIS LINE
+# ================================
 
-answer = run()
-print(answer)
+runCount = 10
+runDurations = []
+
+for count in range(runCount):
+    print(f"Run: {count + 1}")
+    startTime = time()
+    answer = run()
+    runDurations.append(time() - startTime)
+
+averageRuntime = mean(runDurations)
+print(f"Answer: {answer}")
+print(f"Runtime: {round(averageRuntime, 3)}s")
+    
