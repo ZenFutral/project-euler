c7e7b59cbb2c9960544a56d4cfe2fad42d2b0acf
Zen Futral <zen.futral@gmail.com>
2024-11-19 05:31:21 -0700

    Update Problem 3.py

--- Problem 3.py
+++ Problem 3.py
@@ -1,6 +1,7 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 # Answer: 6857
+# Average Runtime: 0.15s
 
 from math import sqrt
 from time import time
